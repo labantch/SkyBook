cceff4a04702e2807aa4cf906ea7e32443a068c8
mohamed serag <Mohamedabdo094@outlook.com>
2026-09-15 15:42:20 +0300

    SeatService and SeatInterface

--- SkyBook.Business/Interfaces/ISeatService.cs
+++ SkyBook.Business/Interfaces/ISeatService.cs
@@ -1,6 +1,10 @@
+using SkyBook.Business.ViewModels;
+
 namespace SkyBook.Business.Interfaces;
 
 public interface ISeatService
 {
-    
+
+    Task<List<SeatSelectionVM>> GetSeatsByFlightAsync(int flightid);
+    Task<bool> IsSeatAvailableAsync(int seatid, int flightid);
 }
