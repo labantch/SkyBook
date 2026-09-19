42e9bcabbfa2bab8935f38232b1f7f6ad3a0300f
Mariam Mohsen <mariammohsen1685@gmail.com>
2026-09-20 00:18:49 +0300

    add http client

--- SkyBook.Presentation/Program.cs
+++ SkyBook.Presentation/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddScoped<SkyBook.Business.Interfaces.IDashboardService, SkyBoo
 builder.Services.AddScoped<SkyBook.Business.Interfaces.IApplicationUserService, SkyBook.Business.Service.ApplicationUserService>();
 builder.Services.AddScoped<SkyBook.Business.Interfaces.IProfileService, SkyBook.Business.Service.ProfileService>();
 builder.Services.AddScoped<SkyBook.Business.Interfaces.IPaymentService, SkyBook.Business.Service.PaymentService>();
-builder.Services.AddScoped<SkyBook.Business.Interfaces.IPaymentGateway, SkyBook.Business.PaymentGateway.PaymobGateway>();
+builder.Services.AddHttpClient<SkyBook.Business.Interfaces.IPaymentGateway, SkyBook.Business.PaymentGateway.PaymobGateway>();
 
 
 
